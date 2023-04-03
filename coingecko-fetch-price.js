f0838dc25e4860f7045124bfcbe58e2ba3a2099f
Lauren Garcia <lauren.garcia@gmail.com>
2023-04-03 13:10:20 -0700

    Update coingecko-fetch-price.js

--- coingecko-fetch-price.js
+++ coingecko-fetch-price.js
@@ -1,11 +1,21 @@
 // Add button to "Custom Scripts" menu in Google Sheets UI
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
-  ui.createMenu('Custom Scripts')
-      // First arg is label, second arg is fn to call (below)
-      .addItem('getCryptoPrices','getCryptoPrices')
-      .addToUi();
+  ui.createMenu('Get Prices')
+    // First arg is label, second arg is fn to call (below)
+    .addItem('getCoinGecko','getCoinGeckoPrices')
+    .addToUi();
 }
+
+
+// TODO:
+// Use Kraken API:
+// https://docs.kraken.com/rest/#tag/Market-Data/operation/getTickerInformation
+// Query List of Assets:
+// https://api.kraken.com/0/public/Assets
+// Ex query: 
+// https://api.kraken.com/0/public/Ticker?pair=ETHUSD,AAVEUSD
+
 
 // CoinGecko API:
 // https://www.coingecko.com/en/api#explore-api
@@ -13,31 +23,43 @@ function onOpen() {
 // Coin id lookup here:
 // https://api.coingecko.com/api/v3/coins/list
 
-coinIdToCell = {
-  'ethereum': 'D4',
-  'the-graph': 'D5',
-  'matic-network': 'D6',
-  'cosmos': 'D7', // atom
-  'terra-luna': 'D8'
+const coinIdToCell = {
+  'D4': { 'CG': 'bitcoin' },
+  'D5': { 'CG': 'ethereum' },
+  'D6': { 'CG': 'the-graph' },
+  'D7': { 'CG': 'matic-network' },
+  'D8': { 'CG': 'cosmos' },
+  'D9': { 'CG': 'terra-luna' },
+  'D10': { 'CG': 'terra-luna2' },
+  'D11': { 'CG': 'curve-dao-token' },
+  'D12': { 'CG': 'uniswap' },
+  'D13': { 'CG': 'truefi' },
+  'D14': { 'CG': 'aave' },
+  'D15': { 'CG': 'radicle' },
+  'D16': { 'CG': 'mantra-dao' },
+  'D17': { 'CG': 'bzx-protocol' },
+  'D18': { 'CG': 'ethereum-name-service' },
 };
 
 function getPricesFromCoinGeckoApi(coinIds) {
-  const _coinIds = Object.keys(coinIdToCell).join(',');
-  //ex: https://api.coingecko.com/api/v3/simple/price?ids=ethereum,matic-network,omisego&vs_currencies=usd
+  const _coinIds = Object.keys(coinIdToCell).map((cell) => coinIdToCell[cell].CG).join(',');
+  // ex: https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,thegraph&vs_currencies=usd
   const queryUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${_coinIds}&vs_currencies=usd`;
   const response = UrlFetchApp.fetch(queryUrl);
   const raw = response.getContentText();
   const data = JSON.parse(raw);
-  Logger.log(data);
   return data;
 }
 
-function getCryptoPrices() {
+function getCoinGeckoPrices() {
   const data = getPricesFromCoinGeckoApi();
   const sheet = SpreadsheetApp.getActiveSheet();
-  Object.keys(coinIdToCell).forEach((coinId) => {
-    const price = data[coinId].usd;  
-    Logger.log(coinId, price, coinIdToCell[coinId]);
-    sheet.getRange(coinIdToCell[coinId]).setValue(price);                            
+  Object.keys(coinIdToCell).forEach((cell) => {
+    const coinId = coinIdToCell[cell].CG;
+    const coinData = data[coinId];
+    const price = coinData ? coinData.usd : 'N/A';
+    const exchange = 'CG';
+    const assetId = coinIdToCell[cell][exchange];
+    sheet.getRange(cell).setValue(price);
   });
 };
